ac0f23f3acee3708a56014af38e9abfa66ca97af
Tvesha Shah <tveshashah13@gmail.com>
2024-02-21 20:17:54 -0500

    set up global variables and added some stop cases closes #1

--- src/main/java/ca/mcmaster/se2aa4/island/team120/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team120/Explorer.java
@@ -11,40 +11,59 @@ import org.json.JSONTokener;
 public class Explorer implements IExplorerRaid {
 
     private final Logger logger = LogManager.getLogger();
+    private Integer batteryLevel; //so we can track battery level 
+    private String action = "stop"; //set to stop for now 
+    private String currentDirection; //so we can know from parsing info what our starter direction is 
 
     @Override
     public void initialize(String s) {
         logger.info("** Initializing the Exploration Command Center");
         JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
         logger.info("** Initialization info:\n {}",info.toString(2));
-        String direction = info.getString("heading");
-        Integer batteryLevel = info.getInt("budget");
-        logger.info("The drone is facing {}", direction);
+
+        currentDirection = info.getString("heading");
+        batteryLevel = info.getInt("budget");
+
+        logger.info("The drone is facing {}", currentDirection);
         logger.info("Battery level is {}", batteryLevel);
     }
 
     @Override
     public String takeDecision() {
         JSONObject decision = new JSONObject();
-        decision.put("action", "stop"); // we stop the exploration immediately
+
+        decision.put("action", action); // we stop the exploration immediately
         logger.info("** Decision: {}",decision.toString());
+
         return decision.toString();
     }
 
     @Override
     public void acknowledgeResults(String s) {
         JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
+        
         logger.info("** Response received:\n"+response.toString(2));
         Integer cost = response.getInt("cost");
+       
         logger.info("The cost of the action was {}", cost);
+        
+        batteryLevel -= cost; 
+        logger.info("Remaining battery{}", batteryLevel);
+
         String status = response.getString("status");
         logger.info("The status of the drone is {}", status);
+        
+        if (batteryLevel==0){
+            action = "stop";
+            deliverFinalReport();
+        }
+       
         JSONObject extraInfo = response.getJSONObject("extras");
         logger.info("Additional information received: {}", extraInfo);
     }
 
     @Override
-    public String deliverFinalReport() {
+    public String deliverFinalReport() {//code does not run to this point? how does bot work exactly
         return "no creek found";
     }
 
