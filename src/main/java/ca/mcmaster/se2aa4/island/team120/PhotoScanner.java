1e7d7404aa673108808553e9517c4bbc2f309082
shivyam <shivyamehta1@gmail.com>
2024-03-17 11:44:52 -0400

    not needed

--- src/main/java/ca/mcmaster/se2aa4/island/team120/PhotoScanner.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team120/PhotoScanner.java
@@ -27,6 +27,8 @@ public class PhotoScanner {
     public boolean isCreek(){
         if(isScanned()){
             if(response.getJSONArray("creeks").length()!=0){
+                JSONArray id_arr = response.getJSONArray("creeks");
+                String id= id_arr.getString(0);
                 return true;
             }
         }
@@ -37,38 +39,18 @@ public class PhotoScanner {
     public boolean isSite(){
         if(isScanned()){
             if(response.getJSONArray("sites").length()!=0){
+                JSONArray id_arr = response.getJSONArray("sites");
+                String id= id_arr.getString(0);
                 return true;
             }
         }
         return false;
     }
 
-    public int getCost(){
-        if(isScanned()){
-            if(response.getJSONArray("cost").length()!=0){
-                return response.getInt("cost");
-            }
-        }
-        return 0;
-    }
 
-    public String getCreek(){
-        if(isCreek()){
-            JSONArray id_arr = response.getJSONArray("creeks");
-            String id= id_arr.getString(0);
-            return id;
-        }
-        return "no creek";
-    }
 
-    public String getSite(){
-        if(isSite()){
-            JSONArray id_arr = response.getJSONArray("sites");
-            String id= id_arr.getString(0);
-            return id;
-        }
-        return "no sites";
-    }
+
+
 
 
 }
