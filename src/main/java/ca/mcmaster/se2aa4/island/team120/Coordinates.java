8bf2a8590a617a0649828594b18e373cbcf6e62f
Tvesha Shah <tveshashah13@gmail.com>
2024-03-17 13:35:15 -0400

    coordinates

--- src/main/java/ca/mcmaster/se2aa4/island/team120/Coordinates.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team120/Coordinates.java
@@ -1,23 +1,31 @@
 package ca.mcmaster.se2aa4.island.team120;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Coordinates {
     private static int x = 0; 
     private static int y = 0; 
 
+    private final Logger logger = LogManager.getLogger();
+
     public void location(String CurrentDirection){
         switch (CurrentDirection){
             case "N":
                 y +=1;
+                logger.info(y);
                 break;
             case "W":
                 x -=1;
+                logger.info(x);
                 break;
             case "S":
                 y -=1;
+                logger.info(y);
                 break;
             case "E":
                 x+=1;
+                logger.info(x);
                 break;
             default:
                 throw new IllegalStateException("INVALID DIRECTION");
