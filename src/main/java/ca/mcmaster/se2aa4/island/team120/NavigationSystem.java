3a9dd5b0cea4063cd9e527126f1bca8230a316d8
Tvesha Shah <tveshashah13@gmail.com>
2024-03-20 15:20:04 -0400

    nav changed

--- src/main/java/ca/mcmaster/se2aa4/island/team120/NavigationSystem.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team120/NavigationSystem.java
@@ -7,10 +7,11 @@ public class NavigationSystem {
 
     FindIsland island = new FindIsland();
     SimpleAlgo run = new SimpleAlgo();
-    Actions action= new Actions();
     
+    Actions action= new Actions();
     Coordinates coords = new Coordinates(); 
     Direction dir = new Direction(); 
+    Data data = new Data(); 
 
     int x; 
     int y; 
@@ -29,17 +30,36 @@ public class NavigationSystem {
         return null;
     }
 
-    public void TopLeft(){
-        int x = coords.x_coords(); 
-        int y = coords.y_coords(); 
-
+    public String TopLeft(){
+        //int x = coords.x_coords(); 
+        //int y = coords.y_coords(); 
+    
         //current position = 0,0 
         //echo up
         //echo down
         //echo left
         //echo right 
-
-
+        int turn = 0; 
+        for (int i=0; i<4; i++){
+            if (i ==0){ 
+                //scan current direction 
+                return action.echo(data.getCurrDirection()); 
+            }else if(i ==1){
+                //echo left 
+                return action.echo(dir.left(data.getCurrDirection())); 
+            }else if (i ==2){
+                //echo right of current
+                return action.echo(dir.right(data.getCurrDirection())); 
+            }else if (i==3 || i ==4){
+                //echo behind of current
+                if (turn ==0){
+                    turn +=1; 
+                    return action.changeDirection(dir.left(data.getCurrDirection()));
+                }else{
+                    return action.echo(dir.left(data.getCurrDirection())); 
+                }
+            }
+        }
         //range in echo up + down = y
         //range in echo left + right = x 
 
